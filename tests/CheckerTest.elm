aee3194cd3907073f27e4714d1ec513903a50a5b
Kevin Li <kevinli020508@gmail.com>
2020-04-19 07:21:29 -0400

    Add tests for intLiteral and indexing

--- tests/CheckerTest.elm
+++ tests/CheckerTest.elm
@@ -1,6 +1,6 @@
 module CheckerTest exposing (suite)
 
-import Test exposing (Test, describe, test)
+import Test exposing (Test, describe)
 import Expect
 import HdlChecker exposing (Type(..), Problem(..))
 import HdlParser exposing (Size(..))
@@ -9,8 +9,8 @@ import AssocList as Dict
 suite : Test
 suite =
   describe "Checker"
-    [ describe "Basics"
-      [ test "Using prelude functions" <|
+    [ describe "Prelude"
+      [ Test.test "Using prelude functions" <|
         \_ ->
           let
             src =
@@ -19,7 +19,9 @@ suite =
               Ok ()
           in
           Expect.equal expected (check src)
-      , test "Omit param bus size and default to 1" <|
+      ]
+    , describe "Bus Size"
+      [ Test.test "Omit param bus size and default to 1" <|
         \_ ->
           let
             src =
@@ -28,7 +30,7 @@ suite =
               Ok ()
           in
           Expect.equal expected (check src)
-      , test "Return type bus size too large" <|
+      , Test.test "Return type bus size too large" <|
         \_ ->
           let
             src =
@@ -37,7 +39,7 @@ suite =
               Err [ MismatchedTypes { from = (1,13), to = (1,16), value = BusType (IntSize 1) } { from = (1,19), to = (1,27), value = BusType (IntSize 2) }]
           in
           Expect.equal expected (check src)
-      , test "Arg type bus size too large" <|
+      , Test.test "Arg type bus size too large" <|
         \_ ->
           let
             src =
@@ -46,7 +48,7 @@ suite =
               Err [MismatchedTypes { from = (1,5), to = (1,6), value = BusType (IntSize 1) } { from = (2,29), to = (2,30), value = BusType (IntSize 2) }]
           in
           Expect.equal expected (check src)
-      , test "Use user-defined functions" <|
+      , Test.test "Use user-defined functions" <|
         \_ ->
           let
             src =
@@ -55,7 +57,7 @@ suite =
               Ok ()
           in
           Expect.equal expected (check src)
-      , test "Variable parameter bus size" <|
+      , Test.test "Variable parameter bus size" <|
         \_ ->
           let
             src =
@@ -64,7 +66,7 @@ suite =
               Ok ()
           in
           Expect.equal expected (check src)
-      , test "VarSize output does not match declared IntSize return type" <|
+      , Test.test "VarSize output does not match declared IntSize return type" <|
         \_ ->
           let
             src =
@@ -73,7 +75,9 @@ suite =
               Err [MismatchedTypes { from = (1,13), to = (1,16), value = BusType (IntSize 1) } { from = (1,19), to = (1,27), value = BusType (VarSize "n" Nothing) }]
           in
           Expect.equal expected (check src)
-      , test "Undefined function name" <|
+      ]
+      , describe "Undefined names"
+      [ Test.test "Undefined function name" <|
         \_ ->
           let
             src =
@@ -82,7 +86,7 @@ suite =
               Err [UndefinedName { from = (1,31), to = (1,34), value = "not" },UndefinedName { from = (1,23), to = (1,26), value = "not" }]
           in
           Expect.equal expected (check src)
-      , test "Undefined function names" <|
+      , Test.test "Undefined function names" <|
         \_ ->
           let
             src =
@@ -91,7 +95,7 @@ suite =
               Err [UndefinedName { from = (3,11), to = (3,14), value = "xor" },UndefinedName { from = (4,13), to = (4,16), value = "and" }]
           in
           Expect.equal expected (check src)
-      , test "Undefined binding name" <|
+      , Test.test "Undefined binding name" <|
         \_ ->
           let
             src =
@@ -100,7 +104,9 @@ suite =
               Err [UndefinedName { from = (1,23), to = (1,24), value = "b" }]
           in
           Expect.equal expected (check src)
-      , test "Record output" <|
+      ]
+      , describe "Records"
+      [ Test.test "Record output" <|
         \_ ->
           let
             src =
@@ -109,7 +115,7 @@ suite =
               Ok ()
           in
           Expect.equal expected (check src)
-      , test "Record output does not match declared record return type" <|
+      , Test.test "Record output does not match declared record return type" <|
         \_ ->
           let
             src =
@@ -121,7 +127,7 @@ suite =
               ]
           in
           Expect.equal expected (check src)
-      , test "Record assignment in locals" <|
+      , Test.test "Record assignment in locals" <|
         \_ ->
           let
             src =
@@ -130,7 +136,7 @@ suite =
               Ok ()
           in
           Expect.equal expected (check src)
-      , test "Undefined name in record literal" <|
+      , Test.test "Undefined name in record literal" <|
         \_ ->
           let
             src =
@@ -140,12 +146,127 @@ suite =
           in
           Expect.equal expected (check src)
       ]
+      , describe "Int literal"
+      [ Test.test "direct assignment of decimal" <|
+        \_ ->
+          let
+            src =
+              "a = 199"
+            expected =
+              Ok ()
+          in
+          Expect.equal expected (check src)
+      , Test.test "direct assignment of hexadecimal" <|
+        \_ ->
+          let
+            src =
+              "a = 0xFF"
+            expected =
+              Ok ()
+          in
+          Expect.equal expected (check src)
+      , Test.test "direct assignment of binary" <|
+        \_ ->
+          let
+            src =
+              "a = 0b1011101"
+            expected =
+              Ok ()
+          in
+          Expect.equal expected (check src)
+      , Test.test "use in record literals" <|
+        \_ ->
+          let
+            src =
+              "r = { a = 0, b = 10 }"
+            expected =
+              Ok ()
+          in
+          Expect.equal expected (check src)
+      , Test.test "use in call" <|
+      \_ ->
+        let
+          src =
+            "a = nand 28 29"
+          expected =
+            Ok ()
+        in
+        Expect.equal expected (check src)
+      , Test.test "mixing different bases" <|
+      \_ ->
+        let
+          src =
+            "a = nand 0x23 0b00100011"
+          expected =
+            Ok ()
+        in
+        Expect.equal expected (check src)
+      ]
+      , describe "Call"
+      [ Test.test "simple call" <|
+        \_ ->
+          let
+            src =
+              "a = nand 0 0"
+            expected =
+              Ok ()
+          in
+          Expect.equal expected (check src)
+      , Test.test "call arity too small" <|
+        \_ ->
+          let
+            src =
+              "a = nand 0"
+            expected =
+              Err [WrongCallArity { from = (1,5), to = (1,9), value = "nand" } [{ name = { from = (-1,-1), to = (-1,-1), value = "a" }, size = { from = (-1,-1), to = (-1,-1), value = VarSize "n" Nothing } },{ name = { from = (-1,-1), to = (-1,-1), value = "b" }, size = { from = (-1,-1), to = (-1,-1), value = VarSize "n" Nothing } }] [{ from = (1,10), to = (1,11), value = BusType (IntSize 1) }]]
+          in
+          Expect.equal expected (check src)
+      , Test.test "call arity too big" <|
+        \_ ->
+          let
+            src =
+              "a = nand 0 0 0"
+            expected =
+              Err [WrongCallArity { from = (1,5), to = (1,9), value = "nand" } [{ name = { from = (-1,-1), to = (-1,-1), value = "a" }, size = { from = (-1,-1), to = (-1,-1), value = VarSize "n" Nothing } },{ name = { from = (-1,-1), to = (-1,-1), value = "b" }, size = { from = (-1,-1), to = (-1,-1), value = VarSize "n" Nothing } }] [{ from = (1,10), to = (1,11), value = BusType (IntSize 1) }, { from = (1,12), to = (1,13), value = BusType (IntSize 1) },{ from = (1,14), to = (1,15), value = BusType (IntSize 1) }]]
+          in
+          Expect.equal expected (check src)
+      ]
+      , describe "Indexing"
+      [ test "Indexing a bus"
+        "head bus[n] -> [1] = bus[0]" <|
+        Ok ()
+      
+      , test "Slicing a bus"
+        "first4 bus[16] -> [4] = bus[0..3]" <|
+        Ok ()
+      
+      ,  test "Index out of bounds"
+        "head bus[2] -> [1] = bus[2]" <|
+        Err [IndexOutOfBounds 2 { from = (1,26), to = (1,27), value = 2 } { from = (1,26), to = (1,27), value = 2 }]
+      
+      , test "end index out of bounds"
+        "first4 bus[3] -> [4] = bus[0..3]" <|
+        Err [IndexOutOfBounds 3 { from = (1,28), to = (1,29), value = 0 } { from = (1,31), to = (1,32), value = 3 }]
+      
+      , test "start index out of bounds"
+        "first4 bus[3] -> [4] = bus[3..4]" <|
+        Err [IndexOutOfBounds 3 { from = (1,28), to = (1,29), value = 3 } { from = (1,31), to = (1,32), value = 4 }]
+      
+      , test "start index greater than end index"
+        "first4 bus[3] -> [2] = bus[2..1]" <|
+        Err [FromIndexBiggerThanToIndex { from = (1,28), to = (1,29), value = 2 } { from = (1,31), to = (1,32), value = 1 }]
+      ]
     ]
+
+test : String -> String -> Result (List HdlChecker.Problem) () -> Test
+test name src expected =
+  Test.test name <|
+    \_ -> Expect.equal expected (check src)
 
 check : String -> Result (List HdlChecker.Problem) ()
 check src =
   case HdlParser.parse src of
-    Err _ -> -- parse error should never happen when testing the checker
-      Err [ UndefinedName <| HdlParser.fakeLocated "Parse Error!!!"]
+    Err deadEnds -> -- parse error should never happen when testing the checker
+      Err [ UndefinedName <| HdlParser.fakeLocated <| HdlParser.showDeadEnds src deadEnds]
     Ok program ->
       HdlChecker.check program
