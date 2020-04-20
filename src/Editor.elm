c92b90a5ce627b99b3dd97fe1cc345f264c9dc2a
Kevin Li <kevinli020508@gmail.com>
2020-04-20 11:50:40 -0400

    Switch sample HDL code to use 4 spaces

--- src/Editor.elm
+++ src/Editor.elm
@@ -175,40 +175,40 @@ init _ =
       """{- sample full adder -}
 
 full_adder a b c -> { sum, carry } =
-  let
-    { sum = s1, carry = c1 } = half_adder a b
-    { sum = s2, carry = c2 } = half_adder s1 c
-    c3 = or c1 c2
-  in
-  { sum = s2, carry = c3 }
+    let
+        { sum = s1, carry = c1 } = half_adder a b
+        { sum = s2, carry = c2 } = half_adder s1 c
+        c3 = or c1 c2
+    in
+    { sum = s2, carry = c3 }
 
 half_adder a b -> { sum, carry } =
-  let
-    sum = xor a b
-    carry = and a b
-  in
-  { sum = sum, carry = carry }
+    let
+        sum = xor a b
+        carry = and a b
+    in
+    { sum = sum, carry = carry }
 
 xor a[n] b[n] -> [n] = -- [n] specifies a variable sized bus
-  let
-    nand_a_b = nand a b
-  in
-  nand
-  (nand a nand_a_b)
-  (nand b nand_a_b)
+    let
+        nand_a_b = nand a b
+    in
+    nand
+    (nand a nand_a_b)
+    (nand b nand_a_b)
 
 or a[n] b[n] -> [n] =
-  nand (not a) (not b)
+    nand (not a) (not b)
 
 not a[n] -> [n] =
-  nand a a
+    nand a a
 
 and a[n] b[n] -> [n] =
-  let
-    nand_a_b = nand a b
-  in
-  nand nand_a_b nand_a_b
-  """
+    let
+        nand_a_b = nand a b
+    in
+    nand nand_a_b nand_a_b
+    """
     hdlOutput = compileHdl hdlSource
   in
   ({ hdlSource = hdlSource
