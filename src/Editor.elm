419752febb2eede6707b61dda5a6d6ceec644ffd
Kevin Li <kevinli020508@gmail.com>
2020-04-20 16:54:25 -0400

    ⭐ Support multiple tabs

--- src/Editor.elm
+++ src/Editor.elm
@@ -7,6 +7,10 @@ import Html.Attributes
 import Element as E
 import Element.Input as Input
 import Element.Font as Font
+import Element.Border as Border
+import Element.Background as Background
+import Element.Events as Events
+import Color
 import HdlParser exposing (Size(..))
 import HdlChecker
 import HdlEmitter
@@ -15,24 +19,43 @@ import Json.Decode as Decode exposing (Decoder)
 import Json.Decode.Field as Field
 import Dict exposing (Dict)
 import Binary
+import Array.NonEmpty as NonEmptyArray exposing (NonEmptyArray)
 
 
 port setEditorValuePort : String -> Cmd msg
 port editorValueChangedPort : (String -> msg) -> Sub msg
 port generateTruthTablePort : Encode.Value -> Cmd msg
 port receiveTruthTablePort : (String -> msg) -> Sub msg
+port changeTabPort : UnitIndex -> Cmd msg
+port addTabPort : UnitIndex -> Cmd msg
 
 
 type alias Model =
-  { hdlSource : String
-  , hdlOutput : Result String (List HdlEmitter.DefOutput)
+  { units : NonEmptyArray Unit
+  , editingActiveUnitName : Bool
+  }
+
+
+type alias Unit =
+  { name : String
+  , source : String
+  , output : Result String (List HdlEmitter.DefOutput)
   , truthTable : TruthTable
   }
+
+
+type alias UnitIndex =
+  Int
 
 
 type Msg
   = EditorValueChanged String
   | TruthTableReceived String
+  | ChangeTab UnitIndex
+  | AddTab
+  | StartEditingActiveUnitName
+  | EditActiveUnitName String
+  | StopEditingActiveUnitName
 
 
 type alias TruthTable =
@@ -42,6 +65,26 @@ type alias TruthTable =
     , outputs : List String
     , body : List (List Int)
     }
+
+
+preludeLength : Int
+preludeLength =
+  1
+
+
+styles =
+  { white = toElmUiColor Color.white
+  , lightGrey = toElmUiColor Color.lightGrey
+  }
+
+
+toElmUiColor : Color.Color -> E.Color
+toElmUiColor color =
+    let
+        {red, green, blue, alpha } =
+            Color.toRgba color
+    in
+    E.rgba red green blue alpha
 
 
 main : Program () Model Msg
@@ -59,29 +102,103 @@ update msg model =
   case msg of
     EditorValueChanged newValue ->
       let
-        hdlOutput =
+        output =
           compileHdl newValue
       in
-      ({ model
-        | hdlSource =
-          newValue
-        , hdlOutput =
-          hdlOutput
-      }
-      , case hdlOutput of
-        Ok defs ->
-          generateTruthTable defs
-        Err _ ->
-          Cmd.none
+      (updateActiveUnit
+        (\activeUnit ->
+          { activeUnit
+          | source =
+            newValue
+          , output =
+            output
+          }
+        )
+        model
+      , generateTruthTable output
       )
     TruthTableReceived tableJson ->
-      ({ model
-        | truthTable =
-          Result.withDefault model.truthTable <| Decode.decodeString decodeTruthTable tableJson
+      (updateActiveUnit
+        (\activeUnit ->
+          { activeUnit
+            | truthTable =
+              Result.withDefault activeUnit.truthTable <| Decode.decodeString decodeTruthTable tableJson
+          }
+        )
+        model
+      , Cmd.none
+      )
+
+    ChangeTab desiredUnitIndex ->
+      (setActiveUnit desiredUnitIndex model, changeTabPort desiredUnitIndex)
+
+    AddTab ->
+      (addUnit "Untitled Unit" model, addTabPort <| NonEmptyArray.length model.units)
+
+    StartEditingActiveUnitName ->
+      ({ model |
+        editingActiveUnitName =
+          True
       }
       , Cmd.none
       )
 
+    EditActiveUnitName newName ->
+      (editActiveUnitName newName model, Cmd.none)
+
+    StopEditingActiveUnitName ->
+      ({ model |
+        editingActiveUnitName =
+          False
+      }
+      , Cmd.none
+      )
+
+
+editActiveUnitName : String -> Model -> Model
+editActiveUnitName newName model =
+  updateActiveUnit
+    (\activeUnit ->
+      { activeUnit |
+        name =
+          newName
+      }
+    )
+    model
+
+
+setActiveUnit : UnitIndex -> Model -> Model
+setActiveUnit desiredUnitIndex model =
+  { model |
+    units =
+      NonEmptyArray.setSelectedIndex desiredUnitIndex <|
+        model.units
+  }
+
+
+addUnit : String -> Model -> Model
+addUnit name model =
+  let
+    newUnit =
+      { name = name
+      , source = "{- Insert HDL code here -}"
+      , output = Ok []
+      , truthTable = Dict.empty
+      }
+  in
+  { model |
+    units =
+      NonEmptyArray.setSelectedIndex (NonEmptyArray.length model.units) <|
+        NonEmptyArray.push newUnit model.units
+  }
+
+
+updateActiveUnit : (Unit -> Unit) -> Model -> Model
+updateActiveUnit updateUnit model =
+  { model |
+    units =
+      NonEmptyArray.updateSelected updateUnit model.units
+  }
 
 
 subscriptions : Model -> Sub Msg
@@ -102,13 +219,83 @@ view model =
     E.column
       []
       [ viewRightPanel model
+      , viewTabSelector model
       ]
+
+
+viewTabSelector : Model -> E.Element Msg
+viewTabSelector model =
+  let
+    _ = Debug.log "AL -> activeUnit" <| activeUnit.name
+    activeUnit =
+      NonEmptyArray.getSelected model.units
+    activeUnitIndex =
+      NonEmptyArray.selectedIndex model.units
+  in
+  E.row
+    [ E.htmlAttribute <| Html.Attributes.style "position" "fixed"
+    , E.htmlAttribute <| Html.Attributes.style "top" "15px"
+    , E.htmlAttribute <| Html.Attributes.style "left" "30px"
+    ] <|
+    NonEmptyArray.toList
+      (NonEmptyArray.indexedMap
+        (\index unit ->
+          if model.editingActiveUnitName && index == activeUnitIndex then
+            Input.text
+              [ Events.onLoseFocus StopEditingActiveUnitName
+              ]
+              { onChange = EditActiveUnitName
+              , text = unit.name
+              , placeholder = Nothing
+              , label = Input.labelHidden "edit active unit name"
+              }
+          else
+            Input.button
+              [ Border.width 1
+              , E.padding 10
+              , if index == activeUnitIndex then
+                Background.color styles.white
+                else
+                Background.color styles.lightGrey
+              ]
+              { onPress =
+                if index == activeUnitIndex then
+                  Just StartEditingActiveUnitName
+                else
+                  Just <| ChangeTab index
+              , label =
+                E.text unit.name
+              }
+        )
+        model.units
+      )
+    ++ [ viewAddTab ]
+
+
+viewAddTab : E.Element Msg
+viewAddTab =
+  Input.button [ Border.width 1, E.padding 10 ]
+    { onPress =
+      Just <| AddTab
+    , label =
+      E.text "+"
+    }
+
 
 viewRightPanel : Model -> E.Element Msg
 viewRightPanel model =
-  case model.hdlOutput of
-    Ok _ -> viewTruthTable model.truthTable
+  let
+    activeUnit =
+      getActiveUnit model
+  in
+  case activeUnit.output of
+    Ok _ -> viewTruthTable activeUnit.truthTable
     Err str -> E.html <| Html.pre [ Html.Attributes.style "white-space" "pre-wrap" ] [ Html.text str ]
+
+
+getActiveUnit : Model -> Unit
+getActiveUnit model =
+  NonEmptyArray.getSelected model.units
 
 
 viewTruthTable : TruthTable -> E.Element Msg
@@ -171,7 +358,7 @@ viewTruthTable table =
 init : () -> ( Model, Cmd Msg )
 init _ =
   let
-    hdlSource =
+    source =
       """{- sample full adder -}
 
 full_adder a b c -> { sum, carry } =
@@ -209,58 +396,67 @@ and a[n] b[n] -> [n] =
     in
     nand nand_a_b nand_a_b
     """
-    hdlOutput = compileHdl hdlSource
+    output = compileHdl source
   in
-  ({ hdlSource = hdlSource
-  , hdlOutput = hdlOutput
-  , truthTable = Dict.empty
+  ({ units =
+    NonEmptyArray.fromElement
+    { name = "Basics"
+    , source = source
+    , output = output
+    , truthTable = Dict.empty
+    }
+  , editingActiveUnitName =
+    False
   }
   , Cmd.batch
-    [ setEditorValuePort hdlSource
-    , case hdlOutput of
-      Ok defs ->
-        generateTruthTable defs
-      Err _ ->
-        Cmd.none
+    [ setEditorValuePort source
+    , generateTruthTable output
     ]
   )
 
-generateTruthTable : List HdlEmitter.DefOutput -> Cmd Msg
-generateTruthTable defs =
-  let
-    -- { name : String
-    -- , params : List ParamOutput
-    -- , outputs : List ParamOutput
-    -- , body : String
-    -- }
-    encodeDefOutput def =
-      Encode.object
-        [ ( "name", Encode.string def.name )
-        , ( "params", Encode.list encodeParamOutput def.params )
-        , ( "outputs", Encode.list encodeParamOutput def.outputs )
-        , ( "body", Encode.string def.body )
-        ]
+generateTruthTable : Result String (List HdlEmitter.DefOutput) -> Cmd Msg
+generateTruthTable output =
+  case output of
+    Err _ ->
+      Cmd.none
+    Ok defs ->
+      if List.length defs > preludeLength then
+        let
+          -- { name : String
+          -- , params : List ParamOutput
+          -- , outputs : List ParamOutput
+          -- , body : String
+          -- }
+          encodeDefOutput def =
+            Encode.object
+              [ ( "name", Encode.string def.name )
+              , ( "params", Encode.list encodeParamOutput def.params )
+              , ( "outputs", Encode.list encodeParamOutput def.outputs )
+              , ( "body", Encode.string def.body )
+              ]
 
-    -- { name : String
-    -- , size : Size
-    -- }
-    encodeParamOutput param =
-      Encode.object
-        [ ( "name", Encode.string param.name )
-        , ( "size", encodeSize param.size )
-        ]
+          -- { name : String
+          -- , size : Size
+          -- }
+          encodeParamOutput param =
+            Encode.object
+              [ ( "name", Encode.string param.name )
+              , ( "size", encodeSize param.size )
+              ]
 
-    -- type Size
-    --   = IntSize Int
-    --   | VarSize String (Maybe Int)
-    encodeSize size =
-      case size of
-        IntSize i ->
-          Encode.int i
-        VarSize n _ ->
-          Encode.string n
-  in
-  generateTruthTablePort <| Encode.list encodeDefOutput defs
+          -- type Size
+          --   = IntSize Int
+          --   | VarSize String (Maybe Int)
+          encodeSize size =
+            case size of
+              IntSize i ->
+                Encode.int i
+              VarSize n _ ->
+                Encode.string n
+        in
+        generateTruthTablePort <| Encode.list encodeDefOutput defs
+      else
+        Cmd.none
 
 -- Dict
 --     String
